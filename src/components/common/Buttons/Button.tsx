b9c8b0bef4dc25a9ded981cec5f4f39669b5e592
jykim29 <skt13181@naver.com>
2025-07-28 16:09:32 +0900

    [feat] : 공통 컴포넌트 설계 - Button
    - 버튼 컴포넌트 설계

---
+++ src/components/common/Buttons/Button.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from 'tailwind-merge';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  type?: 'button' | 'submit' | 'reset';
+  className?: string;
+}
+
+export default function Button({
+  type: buttonType = 'button',
+  className: newClassName,
+  children,
+  ...rest
+}: ButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        'from-primary-100 to-secondary-60 border-primary-60 disabled:from-gray-40 disabled:to-gray-20 disabled:border-gray-40 rounded-xl border bg-linear-0 px-5 py-2 text-white disabled:cursor-not-allowed pointer-fine:hover:not-disabled:brightness-105',
+        newClassName
+      )}
+      type={buttonType}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
