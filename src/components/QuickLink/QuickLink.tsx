44942802f1848471106f38bb6b4ee8e1fc633b11
jykim29 <skt13181@naver.com>
2025-07-30 17:26:04 +0900

    [feat] : 퀵 링크 컴포넌트 추가
    - 필요 정적 애셋 파일 추가
    - 퀵 링크 컴포넌트 추가

---
+++ src/components/QuickLink/QuickLink.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
+
+interface QuickLinkProps extends React.PropsWithChildren {
+  to: string;
+  image?: {
+    src: string;
+    alt: string;
+  };
+  className?: string;
+}
+
+export default function QuickLink({
+  to,
+  image,
+  className: newClassName = '',
+
+  children,
+}: QuickLinkProps) {
+  return (
+    <Link
+      className={twMerge(
+        'bg-secondary-30 border-primary-30 flex items-center justify-center gap-1 rounded-xl border-2 px-2 py-2 pointer-fine:hover:brightness-105',
+        newClassName
+      )}
+      href={to}
+    >
+      {image && (
+        <div className="relative aspect-square h-full">
+          <Image
+            className="object-contain"
+            fill
+            src={image.src}
+            alt={image.alt}
+          />
+        </div>
+      )}
+      <span className="text-button-md w-full text-center break-keep">
+        {children}
+      </span>
+    </Link>
+  );
+}
