5c4faac4a858d3a5bd235bd8330c42dbdaa8f70e
jykim29 <skt13181@naver.com>
2025-08-01 19:29:18 +0900

    [fix] : Next.js 이미지 컴포넌트 prop 오류 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,23 +17,21 @@ interface HeaderProps extends React.PropsWithChildren {
 export default function Header({ left, right, children }: HeaderProps) {
   return (
     <header className="border-b-gray-40 relative h-12 w-full border-b px-5 py-2">
-      <div className="flex items-center justify-center">{children}</div>
+      <div className="flex h-full items-center justify-center">{children}</div>
       {left && (
-        <Link href={left.href}>
-          <Image
-            className="absolute top-1/2 left-5 -translate-y-1/2"
-            src={left.src}
-            alt={left.alt}
-          />
+        <Link
+          className="absolute top-1/2 left-5 -translate-y-1/2"
+          href={left.href}
+        >
+          <Image width={28} height={28} src={left.src} alt={left.alt} />
         </Link>
       )}
       {right && (
-        <Link href={right.href}>
-          <Image
-            className="absolute top-1/2 right-5 -translate-y-1/2"
-            src={right.src}
-            alt={right.alt}
-          />
+        <Link
+          className="absolute top-1/2 right-5 -translate-y-1/2"
+          href={right.href}
+        >
+          <Image width={28} height={28} src={right.src} alt={right.alt} />
         </Link>
       )}
     </header>
