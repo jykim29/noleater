c09d5b081f31d1de8192e635b953cd24f6e597e6
jykim29 <skt13181@naver.com>
2025-08-05 03:01:25 +0900

    [feat] : 메인페이지 배너 캐러셀 컴포넌트 추가
    - 목데이터로 사용

---
+++ src/components/home/MainBannerCarousel.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Image from 'next/image';
+import { Carousel } from '../common';
+import bannerMockData from '@/data/home/banners.json';
+
+export default function MainBannerCarousel() {
+  return (
+    <Carousel autoplayOptions={{ delay: 3000, stopOnInteraction: false }}>
+      <Carousel.Container className="gap-2">
+        {bannerMockData.data.map((item) => (
+          <Carousel.Slide
+            key={item.id}
+            className="relative aspect-[1/0.4] w-full rounded-lg"
+          >
+            <Image
+              className="object-cover"
+              src={item.imgSrc}
+              alt={item.alt}
+              fill
+              sizes="100vw"
+            />
+          </Carousel.Slide>
+        ))}
+      </Carousel.Container>
+      <Carousel.PageIndicator
+        type="dotted"
+        onClick={(el, emblaApi) => {
+          const idx = Number(el.dataset.slideIdx);
+          if (!Number.isNaN(idx)) emblaApi.scrollTo(idx);
+        }}
+      />
+    </Carousel>
+  );
+}
