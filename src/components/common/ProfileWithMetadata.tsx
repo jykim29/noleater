f342431c98c7aaa773efc93020e0000b28ad8857
jykim29 <skt13181@naver.com>
2025-08-04 13:07:15 +0900

    [feat] : 프로필메타데이터 컴포넌트 추가

---
+++ src/components/common/ProfileWithMetadata.tsx
@@ -0,0 +1,53 @@
+import ProfileImage from './ProfileImage';
+import IconWithCount from '../shared/IconWithCount';
+
+interface ProfileSetProps {
+  profile: {
+    imgSrc: string;
+    username: string;
+  };
+  metadata: {
+    views?: number;
+    comments?: number;
+    like?: number;
+  };
+}
+
+export default function ProfileWithMetadata({
+  profile: { imgSrc, username },
+  metadata,
+}: ProfileSetProps) {
+  return (
+    <div className="flex items-center gap-3">
+      <ProfileImage src={imgSrc} username={username} />
+      <div>
+        <span>프로필이름</span>
+        {metadata && (
+          <div className="flex items-center gap-2">
+            {metadata.views && (
+              <IconWithCount
+                icon={{ src: '/assets/icons/eyes.svg', alt: '조회수' }}
+              >
+                <span className="text-gray-80">{metadata.views}</span>
+              </IconWithCount>
+            )}
+            {metadata.comments && (
+              <IconWithCount
+                icon={{ src: '/assets/icons/comment.svg', alt: '댓글수' }}
+              >
+                <span className="text-gray-80">{metadata.comments}</span>
+              </IconWithCount>
+            )}
+            {metadata.like && (
+              <IconWithCount
+                icon={{ src: '/assets/icons/thumbs_up.svg', alt: '좋아요 수' }}
+              >
+                <span className="text-gray-80">{metadata.like}</span>
+              </IconWithCount>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
