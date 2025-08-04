3c090ae354b4756ceb70304084a83fbfed1cf377
jykim29 <skt13181@naver.com>
2025-08-05 02:48:58 +0900

    [feat] : Embla 캐러셀 페이지네이션 커스텀 훅 추가
    - 페이지네이션의 편의를 위해 현재 슬라이드 인덱스와 총 슬라이드 개수를 리턴하는 커스텀 훅 추가

---
+++ src/hooks/useEmblaPagination.ts
@@ -0,0 +1,34 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { EmblaCarouselType } from 'embla-carousel';
+
+export default function useEmblaPagination(emblaApi?: EmblaCarouselType) {
+  const [index, setIndex] = useState<number>(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+
+  const handleSelect = useCallback((emblaApi?: EmblaCarouselType) => {
+    if (!emblaApi) return;
+    const currentIndex = emblaApi.selectedScrollSnap();
+    setIndex((prev) => {
+      return prev === currentIndex ? prev : currentIndex;
+    });
+  }, []);
+
+  useEffect(() => {
+    if (emblaApi) {
+      const initialize = (emblaApi: EmblaCarouselType) => {
+        const snapList = emblaApi.scrollSnapList();
+        setScrollSnaps(snapList);
+      };
+      initialize(emblaApi);
+      emblaApi.on('select', handleSelect);
+    }
+
+    return () => {
+      if (emblaApi) emblaApi.off('select', handleSelect);
+    };
+  }, [emblaApi, handleSelect]);
+
+  return [index, scrollSnaps.length];
+}
