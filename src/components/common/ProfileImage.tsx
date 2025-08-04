ef416d66809d457eeefe5320de2864215d92eb61
jykim29 <skt13181@naver.com>
2025-08-04 12:24:43 +0900

    [feat] : 프로필이미지 컴포넌트 추가

---
+++ src/components/common/ProfileImage.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
+
+interface ProfileImageProps {
+  className?: string;
+  username?: string;
+  src?: string;
+}
+
+export default function ProfileImage({
+  className: newClassName = '',
+  src,
+  username = '',
+}: ProfileImageProps) {
+  return (
+    <div
+      className={twMerge(
+        'relative h-12 w-12 flex-none overflow-hidden rounded-full',
+        newClassName
+      )}
+    >
+      <Image
+        className="object-cover"
+        fill
+        sizes="100vw"
+        src={src ?? '/assets/images/default_profile.jpg'}
+        alt={username}
+      />
+    </div>
+  );
+}
