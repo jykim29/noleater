d4cc5c6503184bb24937d3cb53cc6e61529acbf8
jykim29 <skt13181@naver.com>
2025-08-07 22:14:05 +0900

    [refactor] : 컨텍스트 커스텀 훅 에러메세지 변경

--- src/contexts/CarouselContext.tsx
+++ src/contexts/CarouselContext.tsx
@@ -13,7 +13,7 @@ export const useCarouselContext = () => {
   const context = useContext(CarouselContext);
   if (!context)
     throw new Error(
-      'useCarouselContext훅은 Carousel 컴포넌트 내부에서만 사용되어야 합니다.'
+      'useCarouselContext훅은 CarouselContextProvider 컴포넌트 내부에서만 사용되어야 합니다.'
     );
   return context;
 };
