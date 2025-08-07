3eef18c6b0c126f0177a5979b37d23294f50513a
jykim29 <skt13181@naver.com>
2025-08-07 15:10:19 +0900

    [add] : 메인배너캐러셀 컴포넌트 엔트리파일에 추가

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,4 +1,4 @@
-export { default as Carousel } from './Carousel';
 export { default as MainSection } from './MainSection';
 export { default as NoticeBar } from './NoticeBar';
 export { default as QuickLink } from './QuickLink';
+export { default as MainBannerCarousel } from './MainBannerCarousel';
