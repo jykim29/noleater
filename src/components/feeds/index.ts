129caba0f8b66e49d80e410c608babdeb6873e88
jykim29 <skt13181@naver.com>
2025-08-07 14:20:11 +0900

    [add] : 피드 컴포넌트 엔트리파일 추가

---
+++ src/components/feeds/index.ts
@@ -0,0 +1,5 @@
+export { default as FeedHeader } from './FeedHeader';
+export { default as FeedItem } from './FeedItem';
+export { default as FeedCarousel } from './FeedCarousel';
+export { default as FeedList } from './FeedList';
+export { default as FeedGrid } from './FeedGrid';
