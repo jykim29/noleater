f1add986916de9c82c71040aab9a17758f97bd75
jykim29 <skt13181@naver.com>
2025-08-07 14:50:47 +0900

    [feat] : 오버레이 컴포넌트 추가

---
+++ src/components/common/Overlay.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { twMerge } from 'tailwind-merge';
+
+interface OverlayProps {
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+export default function Overlay({
+  className: newClassName,
+  onClick: handleClick,
+  children,
+}: React.PropsWithChildren<OverlayProps>) {
+  return (
+    <div
+      className={twMerge(
+        'bg-overlay absolute top-0 left-1/2 z-[9999] h-screen w-full max-w-md min-w-xs -translate-x-1/2',
+        newClassName
+      )}
+      onClick={handleClick}
+    >
+      {children}
+    </div>
+  );
+}
