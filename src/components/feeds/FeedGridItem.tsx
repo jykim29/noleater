6e0f9e2edc9a1d87ab31f0c418f5884c4d92d316
jykim29 <skt13181@naver.com>
2025-08-10 10:52:36 +0900

    [feat] : 피드 그리드 아이템 컴포넌트 추가

---
+++ src/components/feeds/FeedGridItem.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import Image from 'next/image';
+import FeedItem from './FeedItem';
+import { Button, GridItem } from '../common';
+import { useModalContext } from '@/contexts/ModalContext';
+
+interface FeedGridItemProps {
+  id: number;
+  imgSrc: string;
+  isMultiple?: boolean;
+}
+
+export default function FeedGridItem({
+  id,
+  imgSrc,
+  isMultiple = false,
+}: FeedGridItemProps) {
+  const { open } = useModalContext();
+  const handleClick = (id: number) => {
+    open.centerModal(<FeedItem />);
+  };
+  return (
+    <GridItem>
+      <Button
+        type="button"
+        className="relative block h-full w-full border-none bg-none p-0"
+        onClick={() => handleClick(id)}
+      >
+        <Image
+          className="object-cover"
+          src={imgSrc}
+          alt=""
+          fill
+          sizes="100vw"
+        />
+        {isMultiple && (
+          <Image
+            className="absolute top-2 right-2"
+            src="/assets/icons/multiple_image.svg"
+            alt="이미지 더있음"
+            width={21}
+            height={20}
+          />
+        )}
+      </Button>
+    </GridItem>
+  );
+}
