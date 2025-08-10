762abde67b31017250b583bb88a1531828925950
jykim29 <skt13181@naver.com>
2025-08-10 10:53:18 +0900

    [refactor] : 피드 그리드 컴포넌트 코드 리팩토링

--- src/components/feeds/FeedGrid.tsx
+++ src/components/feeds/FeedGrid.tsx
@@ -1,31 +1,14 @@
-import Image from 'next/image';
-import { Button, GridItem } from '../common';
+import FeedGridItem from './FeedGridItem';
 
 export default function FeedGrid() {
   return (
     <div className="grid grid-cols-3 gap-1">
       {[...Array(128)].map((_, idx) => (
-        <GridItem key={idx}>
-          <Button
-            type="button"
-            className="relative block h-full w-full border-none bg-none p-0"
-          >
-            <Image
-              className="object-cover"
-              src={`https://picsum.photos/256/256?random=${idx}`}
-              alt={`테스트이미지-${idx}`}
-              fill
-              sizes="100vw"
-            />
-            <Image
-              className="absolute top-2 right-2"
-              src="/assets/icons/multiple_image.svg"
-              alt="이미지 더있음"
-              width={21}
-              height={20}
-            />
-          </Button>
-        </GridItem>
+        <FeedGridItem
+          key={idx}
+          id={idx}
+          imgSrc={`https://picsum.photos/300/200?random=${idx}`}
+        />
       ))}
     </div>
   );
