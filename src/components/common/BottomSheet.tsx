b497ad23fbfbfdd060eb2460f7489d16acbb6d51
jykim29 <skt13181@naver.com>
2025-08-16 18:45:44 +0900

    [design] : 스타일 속성값 변경

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -10,7 +10,7 @@ export default function BottomSheet({
       <div className="flex h-6 w-full items-center justify-center">
         <span className="bg-gray-40 h-1 w-14 rounded-full"></span>
       </div>
-      <div className="px-7 pt-4 pb-7">{children}</div>
+      <div className="px-12 pt-4 pb-7">{children}</div>
     </div>
   );
 }
