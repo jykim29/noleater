6753c6a02c2608966dc6c1bb418504c614f17690
jykim29 <skt13181@naver.com>
2025-08-17 20:28:55 +0900

    [feat] : 랜딩 페이지 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default async function Home() {
+import Image from 'next/image';
+import Link from 'next/link';
+import AnimatedBackground from '@/components/landing/AnimatedBackground';
+
+export default async function Landing() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <AnimatedBackground>
+      <main className="flex h-full w-full flex-col items-center justify-center gap-32">
+        <Image
+          src="https://placehold.jp/150x150.png"
+          width={150}
+          height={150}
+          alt="놀잇터 로고"
+        />
+        <Link
+          className="from-primary-100 to-secondary-60 border-primary-60 disabled:from-gray-40 disabled:to-gray-20 disabled:border-gray-40 text-button-md rounded-full border bg-linear-0 px-5 py-2 text-white disabled:cursor-not-allowed pointer-fine:hover:not-disabled:brightness-95"
+          href="/login"
+        >
+          입장하기
+        </Link>
+      </main>
+    </AnimatedBackground>
   );
 }
