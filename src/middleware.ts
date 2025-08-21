de40c4737e3bdd1701cb9b0dc9b2c487d589c802
jykim29 <skt13181@naver.com>
2025-08-21 16:30:15 +0900

    [remove] : 불필요한 코드 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from './libs/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
   return await updateSession(request);
 }
 
