ae3dd9c2cd3ff9152d32c2cd5180c132888d9999
jykim29 <skt13181@naver.com>
2025-08-24 03:33:19 +0900

    [add] : supabase client에 데이터베이스 타입 명시

--- src/libs/supabase/server.ts
+++ src/libs/supabase/server.ts
@@ -1,9 +1,10 @@
-import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
+import { Database } from '@/../database.types';
 
 export async function createClient() {
   const cookieStore = await cookies();
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
