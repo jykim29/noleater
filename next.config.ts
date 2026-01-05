d067f134e40287c5bd18b37a0085b158ae326a74
jykim29 <skt13181@naver.com>
2026-01-05 14:41:37 +0900

    [add] : 패키지 업데이트 및 프로젝트 환경 설정
    - react-dom 패키지 업데이트 (19.1.4)
    - 개발서버 https 프로토콜 적용
    - next.js 환경설정의 이미지에 supabase url 추가

--- next.config.ts
+++ next.config.ts
@@ -60,6 +60,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL as string).host,
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
